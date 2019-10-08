1099e922fe53efad767181a815a89a49f258b928
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-10-07 19:44:50 -0500

    added get route to server js

--- server.js
+++ server.js
@@ -49,7 +49,11 @@ if (process.env.NODE_ENV === 'production') {
   // set static folder
   app.use(express.static('client/build'));
 }
-
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 // Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
